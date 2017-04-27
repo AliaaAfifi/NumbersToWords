de8fc725b6ec589ffc5c855186b50df3fc0f1241
aliaa <aliaaadelafifi@gmail.com>
2017-04-28 00:52:43 +0200

    Code Review Fixing

--- src/Numbers/NumbersToEnglishWords.scala
+++ src/Numbers/NumbersToEnglishWords.scala
@@ -3,9 +3,10 @@ import scala.math.BigDecimal
 /**
   * Created by aliaa on 20/04/17.
   */
-class NumbersToEnglishWords {
+object NumbersToEnglishWords {
   private val tensNames = Array("", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")
   private val numNames = Array("", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
+  private val specialNames = Array("", "thousand", "million")
 
   private def convertLessThanOneThousand(number: Int) = {
     //Splitting the three digits
@@ -14,13 +15,12 @@ class NumbersToEnglishWords {
     val hundreds: Int = number / 100
 
     //Converting unit and tens into words
-    val soFar = (number % 100 < 20) match {
-      case true => numNames(number % 100)
-      case false => {
-        numNames(number % 10) match {
-          case "" => tensNames(tens) + numNames(number % 10)
-          case _ => tensNames(tens) + "-" + numNames(number % 10)
-        }
+    val soFar = if (number % 100 < 20) {
+      numNames(number % 100)
+    } else {
+      numNames(number % 10) match {
+        case "" => tensNames(tens) + numNames(number % 10)
+        case _ => tensNames(tens) + "-" + numNames(number % 10)
       }
     }
 
@@ -38,42 +38,20 @@ class NumbersToEnglishWords {
   private def convertBeforeDecimalPoint(I_Number: Int): String = I_Number match {
     case 0 => "zero"
     case _ => {
-      //Padding the number to 9 digits
-      val paddedNumber = f"${I_Number}%09d"
-
-      //Splitting the number
-      val Millions_inNumbers = paddedNumber.substring(0, 3)
-      val HundredThousands_inNumbers = paddedNumber.substring(3, 6)
-      val Thousands_inNumbers = paddedNumber.substring(6, 9)
-
-      //Converting the parts into words
-      val Millions_inWords = Millions_inNumbers match {
-        case "000" => ""
-        case _ => convertLessThanOneThousand(Integer.parseInt(Millions_inNumbers)) + " million, "
-      }
-
-      val HundredThousands_inWords = HundredThousands_inNumbers match {
-        case "000" => ""
-        case _ => convertLessThanOneThousand(Integer.parseInt(HundredThousands_inNumbers)) + " thousand, "
-      }
-
-      val Thousands_inWords = convertLessThanOneThousand(Integer.parseInt(Thousands_inNumbers.toString))
-
-      //Conacatenating string of each part
-      val resultWord = (Millions_inWords + HundredThousands_inWords + Thousands_inWords).trim
-
-      //Dropping the last character(if it is comma), then returning the result word
-      val commaCheck = resultWord takeRight(1)
+      val resultWord = convert(I_Number, 0).trim
+      val commaCheck = resultWord takeRight (1)
       commaCheck match {
-        case "," => resultWord dropRight(1)
-        case _ => resultWord
+        case "," => (resultWord dropRight (1)).trim
+        case _ => resultWord.trim
       }
     }
+
   }
 
   private def convertAfterDecimalPoint(D_Number: Double): String = D_Number match {
     case 0.25 => "twenty-five"
     case 0.5 => "fifty"
+    case 0.75 => "seventy-five"
     case _ => ""
   }
 
@@ -84,13 +62,23 @@ class NumbersToEnglishWords {
     (beforeDecimalPoint, AfterDecimalPoint.toDouble)
   }
 
+  private def convert(theNum: Int, index: Int): String = {
+    if (theNum == 0) {
+      ""
+    } else {
+      val part = theNum % 1000
+      val theWord = part match {
+        case 0 => ""
+        case _ => convertLessThanOneThousand(part) + " " + specialNames(index) + ", "
+      }
+      convert(theNum / 1000, index + 1) + theWord
+    }
+  }
+
   def convertToWords(D_Number: BigDecimal): String = {
-    val pairOfTwoNums = preConversion(D_Number)
-    val inPounds = pairOfTwoNums._1
-    val inPiasters = pairOfTwoNums._2
+    val (inPounds, inPiasters) = preConversion(D_Number)
 
     val pound: String = inPounds match {
-      case 0 => ""
       case 1 => " pound"
       case _ => " pounds"
     }
@@ -100,5 +88,26 @@ class NumbersToEnglishWords {
     } else {
       convertBeforeDecimalPoint(inPounds) + pound + ", " + convertAfterDecimalPoint(inPiasters) + " piasters"
     }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(convertToWords(10))
+    println(convertToWords(100))
+    println(convertToWords(1000))
+    println(convertToWords(1000000))
+    println(convertToWords(9))
+    println(convertToWords(99))
+    println(convertToWords(999))
+    println(convertToWords(9999))
+    println(convertToWords(99999))
+    println(convertToWords(999999))
+    println(convertToWords(101))
+    println(convertToWords(111))
+    println(convertToWords(1001))
+    println(convertToWords(1101))
+    println(convertToWords(11001))
+    println(convertToWords(1111))
+    println(convertToWords(1011))
+    println(convertToWords(111101))
   }
 }
